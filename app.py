9bb29b84df83c08856af19bfcdbfe14cfbb0491d
Brent Ochieng <brentwash35@gmail.com>
2026-05-20 18:13:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ st.markdown("""
 
 /* Subtitle Container */
 .subtitle-container {
-    background-color: #145a32;
+    background-color: #1b3321;
     padding: 1rem;
     border-radius: 12px;
     margin-bottom: 2rem;
